d562fec49d49acedb2dfa93cde960303737787b4
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-11-09 13:50:45 +0400

    Add `unstd.templates` module

---
+++ unstd/templates.d
@@ -0,0 +1,9 @@
+﻿/** Vriouse stuff for working with _templates.
+
+Copyright: Denis Shelomovskij 2011-2012
+
+License: $(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Denis Shelomovskij
+*/
+module unstd.templates;
