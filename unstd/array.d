339e038c205ab9defbcb6dad40262c80bf997942
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-11-14 09:49:58 +0400

    Add `unstd.array` module

---
+++ unstd/array.d
@@ -0,0 +1,12 @@
+﻿/** Additions to std.array.
+
+Copyright: Denis Shelomovskij 2011-2012
+
+License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Denis Shelomovskij
+*/
+module unstd.array;
+
+
+public import std.array;
