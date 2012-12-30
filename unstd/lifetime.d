1a5d6fb9355a6a43ae44781c558bdf158cab26ad
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-12-30 10:26:22 +0400

    Add `unstd.lifetime` module

---
+++ unstd/lifetime.d
@@ -0,0 +1,9 @@
+﻿/** Functions for user-defined _lifetime implementation.
+
+Copyright: Denis Shelomovskij 2012
+
+License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Denis Shelomovskij
+*/
+module unstd.lifetime;
