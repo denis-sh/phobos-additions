bbd534b6d9651e1c9421d84e98df115bbb5b9a80
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2013-04-17 21:01:07 +0400

    Helper functions for memory amound manipulation.

--- unstd/memory/allocation.d
+++ unstd/memory/allocation.d
@@ -50,3 +50,39 @@ unittest
 	foreach(alignment; [1, 4, 16, 64, 128, 256])
 		assert(alignMemory(alignment, buff.ptr) != buff.ptr);
 }
+
+
+// Helper functions for memory amound manipulation.
+// ----------------------------------------------------------------------------------------------------
+
+size_t memoryAdd(size_t bytes1, size_t bytes2)
+in { assert(bytes1 || bytes2); }
+body
+{
+	const size_t bytes = bytes1 + bytes2;
+	if(bytes < bytes1)
+		return 0;
+	return bytes;
+}
+
+size_t memoryMult(size_t elementSize, size_t count)
+in { assert(elementSize && count); }
+body
+{
+	const size_t bytes = elementSize * count;
+	if(bytes / elementSize != count)
+		return 0;
+	return bytes;
+}
+
+unittest
+{
+	assert( memoryAdd(3, 4) == 7);
+	assert( memoryAdd(1, 0) == 1);
+	assert( memoryAdd(2, -3) == size_t.max);
+	assert(!memoryAdd(3, -1));
+	assert( memoryMult(3, 4) == 12);
+	assert( memoryMult(3, 1) == 3);
+	assert( memoryMult(1, -1) == size_t.max);
+	assert(!memoryMult(3, -1));
+}
