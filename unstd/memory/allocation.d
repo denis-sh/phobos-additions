586e87821f19f1756d2c53898f854394256e5158
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2013-04-17 20:56:33 +0400

    Add helper functions for memory alignment.

--- unstd/memory/allocation.d
+++ unstd/memory/allocation.d
@@ -7,3 +7,46 @@ License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
 Authors: Denis Shelomovskij
 */
 module unstd.memory.allocation;
+
+
+import core.stdc.stdlib;
+
+import unstd.math;
+
+
+private:
+
+// Helper functions for memory alignment.
+// Note: maximum allowed alignment is 256.
+// ----------------------------------------------------------------------------------------------------
+
+size_t alignmentMemoryPadding(size_t alignment) pure @safe
+{ return alignment; }
+
+void* alignMemory(size_t alignment, void* unalignedPtr) pure
+in { assert(unalignedPtr && alignment && alignment <= ubyte.max + 1); }
+out(res) { assert(dealignMemory(res) == unalignedPtr); }
+body
+{
+	void* ptr = cast(void*) alignUp(alignment, cast(size_t) unalignedPtr);
+	if(ptr == unalignedPtr)
+		ptr += alignment;
+
+	(cast(ubyte*) ptr)[-1] = cast(ubyte)(ptr - unalignedPtr - 1);
+	return ptr;
+}
+
+inout(void)* dealignMemory(inout void* alignedPtr) pure
+in { assert(alignedPtr); }
+body
+{
+	const d = (cast(ubyte*) alignedPtr)[-1] + 1;
+	return alignedPtr - d;
+}
+
+unittest
+{
+	void[alignmentMemoryPadding(256)] buff = void;
+	foreach(alignment; [1, 4, 16, 64, 128, 256])
+		assert(alignMemory(alignment, buff.ptr) != buff.ptr);
+}
