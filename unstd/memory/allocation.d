bd5efd8f2efb0c04ebd5dadadfb588821acb0ad9
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2013-04-17 20:51:08 +0400

    Add `unstd.memory.allocation` module

---
+++ unstd/memory/allocation.d
@@ -0,0 +1,9 @@
+﻿/** Manual memory management routines.
+
+Copyright: Denis Shelomovskij 2013
+
+License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Denis Shelomovskij
+*/
+module unstd.memory.allocation;
