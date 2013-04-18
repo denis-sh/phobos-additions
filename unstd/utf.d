8f946edebc28d81f5cf024db45d670994c418f77
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2013-04-18 11:17:15 +0400

    Add `unstd.utf` module

---
+++ unstd/utf.d
@@ -0,0 +1,12 @@
+﻿/** Additions to $(STDMODULE _utf).
+
+Copyright: Denis Shelomovskij 2013
+
+License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Denis Shelomovskij
+*/
+module unstd.utf;
+
+
+public import std.utf;
