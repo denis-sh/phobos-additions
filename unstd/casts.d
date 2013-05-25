226fdafd4a18ed74c56903d9fa198c17fe2bb0a7
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2013-05-25 10:52:42 +0400

    Add `unstd.casts` module.

---
+++ unstd/casts.d
@@ -0,0 +1,10 @@
+﻿/**
+Functions for casts.
+
+Copyright: Denis Shelomovskij 2013
+
+License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Denis Shelomovskij
+*/
+module unstd.casts;
