d97a6bf7735ccbc97aa23e16e4f1402ac94943bf
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2013-06-03 14:29:12 +0400

    Add `unstd.memory.misc` module.

---
+++ unstd/memory/misc.d
@@ -0,0 +1,10 @@
+﻿/**
+Miscellaneous memory routines.
+
+Copyright: Denis Shelomovskij 2013
+
+License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Denis Shelomovskij
+*/
+module unstd.memory.misc;
