ad6167fdcb0247e9a9b2352676e51df38fb65477
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2013-06-18 13:12:55 +0400

    Add check whether CTFE bug if fixed in `unstd.array` unittest.

--- unstd/array.d
+++ unstd/array.d
@@ -255,7 +255,12 @@ unittest
 			ubyte[2] srcArr = [1, 2], destArr;
 			f!void(srcArr.ptr, destArr.ptr, 2);
 			// `destArr` is not changed in CT because of dmd @@@BUG10243@@@
-			if(!__ctfe) assert(destArr == [1, 2]);
+			if(__ctfe)
+			{
+				assert(destArr == [0, 0], "CTFE bug fixed. Remove this workaround.");
+				destArr = [1, 2];
+			}
+			assert(destArr == [1, 2]);
 		}
 	}
 	test!rawCopyCTImpl(); // Test CT variant at RT
