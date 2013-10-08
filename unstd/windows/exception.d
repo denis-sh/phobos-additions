86764193a281804e7b1478acd8ef3fa27ae7020b
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2013-10-08 20:18:03 +0400

    Replace *.d files with redirection message.
    Also remove all other files.

--- unstd/windows/exception.d
+++ unstd/windows/exception.d
@@ -1,61 +1,9 @@
-﻿/** Functions for throwing exceptions on WinAPI errors.
+﻿/*
+                                                 The project is moved to
+                                                        Bitbucket
+                                                      and renamed to
+                                                        Unstandard
 
-Copyright: Denis Shelomovskij 2013
-
-License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
-
-Authors: Denis Shelomovskij
+                                          So go to new location using this URL:
+                                        https://bitbucket.org/denis-sh/unstandard
 */
-module unstd.windows.exception;
-
-
-import core.sys.windows.windows;
-
-import std.traits;
-
-version(unittest)
-{
-	import std.algorithm;
-	import std.exception;
-}
-
-
-/**
-Exception thrown on WinAPI errors.
-*/
-class WinAPIException: Exception
-{
-	this(string functionName, string file = __FILE__, size_t line = __LINE__, Throwable next = null) @safe pure nothrow
-	{ super("WinAPI function '" ~ functionName ~ "' failed.", file, line, next); }
-}
-
-/**
-Convinient template to call WinAPI function and throw $(MREF WinAPIException)
-on error.
-*/
-template enforceWinAPI(alias func)
-if(functionLinkage!func == "Windows")
-{
-	auto enforceWinAPI(ParameterTypeTuple!func args, string file = __FILE__, size_t line = __LINE__)
-	{
-		if(auto res = func(args))
-			return res;
-		throw new WinAPIException(__traits(identifier, func), file, line);
-	}
-}
-
-///
-unittest
-{
-	HANDLE processHeap = enforceWinAPI!GetProcessHeap();
-	assert(processHeap); // Will always pass.
-}
-
-unittest
-{
-	assert(enforceWinAPI!GetProcessHeap() == GetProcessHeap());
-	assert(collectExceptionMsg!WinAPIException(enforceWinAPI!CloseHandle(null)).canFind("CloseHandle"));
-}
-
-version(unittest)
-extern(Windows) nothrow extern HANDLE GetProcessHeap();
